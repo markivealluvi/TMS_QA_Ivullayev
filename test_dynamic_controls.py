04cf42b30fcf39478f3ca996b608349e4259682e
markivealluvi <ikram.sharipoff@gmail.com>
2022-07-03 09:31:48 +0300

    Домашка 01.07.22|Задание 1

---
+++ test_dynamic_controls.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_dynamic_controls():
+
+    chrome = webdriver.Chrome('./chromedriver')
+    chrome.implicitly_wait(5)
+
+    try:
+
+        url = 'http://the-internet.herokuapp.com/dynamic_controls'
+        chrome.get(url)
+
+        chrome.find_element(By.CSS_SELECTOR, '[type="checkbox"]').click()
+        chrome.find_element(By.CSS_SELECTOR, '[onclick="swapCheckbox()"]').click()
+        chrome.find_element(By.ID, 'message')
+        WebDriverWait(chrome, 5).until(EC.visibility_of_element_located((By.ID, 'message')))
+        WebDriverWait(chrome, 5).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '[type="checkbox"]')))
+
+        input_text = chrome.find_element(By.CSS_SELECTOR, '[type="text"]')
+        assert input_text.is_enabled() is False
+
+        chrome.find_element(By.CSS_SELECTOR, '[onclick="swapInput()"]').click()
+        WebDriverWait(chrome, 5).until(EC.visibility_of_element_located((By.XPATH, '//form[@id="input-example"]/p')))
+        assert input_text.is_enabled() is True
+
+    finally:
+        chrome.quit()
